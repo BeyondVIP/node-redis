8d8764114ce1d17d888fda2b5ab03d4f3efa80af
Viatcheslav Mogilevsky <triggerwoods91@gmail.com>
2014-04-17 00:29:37 -0700

    Reset nginx timeout

--- foreman_export/production.sh
+++ foreman_export/production.sh
@@ -1,3 +1,4 @@
 echo "NODE_ENVIRONMENT=production" > .env
 mkdir -p log/
 nf export  -o /etc/init/ -a node-redis -l `pwd`/log/ -p 8814 web=4
+nf export  -o /etc/init/ -a node-redis-keeper -l `pwd`/log/
